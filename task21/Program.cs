4435ccdce27dfe6dfd70b6158b226bed3af5bcc4
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-27 18:38:04 +0300

    tasks 19, 21, 23 added

---
+++ task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+Console.WriteLine("Enter A and B coordinates and I will calculate the distance between them.");
+Console.WriteLine("Enter xA");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter yA");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter xB");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter yB");
+int yB = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Sqrt((Math.Pow((xB-xA), 2))+(Math.Pow((yB-yA), 2)));
+Console.WriteLine("Distance equals " + Math.Round(distance, 3));
---
+++ task23/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Enter any number and I will print all the squareroots of the numbers between 1 to it");
+int end = Convert.ToInt32(Console.ReadLine());
+// for(int i =1, i <= end, i++);
+// {
+//     Console.Write(Math.Pow(i, 2) + ", ");
+// }
