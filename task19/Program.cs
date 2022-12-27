b32c1587f0a3a36ffe746900b4bfc1ceb5877bba
Valentine Borisevich <valentineborisevich@gmail.com>
2022-12-27 19:42:30 +0300

    task 19 in the middle

--- task19/Program.cs
+++ task19/Program.cs
@@ -5,3 +5,17 @@
 // 12821 -> да
 
 // 23432 -> да
+Console.WriteLine("Enter five digit number and I will inform you if it's a palindrome number");
+int a = Convert.ToInt32(Console.ReadLine());
+string aStr = Convert.ToString(a);
+if (a >= 10000 && a < 100000)
+{
+    if(aStr[0] == aStr[4] && aStr[1] == aStr[3])
+{
+    Console.WriteLine("This is palindrome number");
+}
+else
+{
+    Console.WriteLine("This is not palindrome number");
+}
+}
